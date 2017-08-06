83cf7fdcb62d428e5d374f8b18ce4e65fb75519e
Harvey Magana <heyharv2012@gmail.com>
2017-08-06 09:44:29 -0700

    second commit, completed code for nicknames

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
-	this.level = ko.observable('');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
 	this.catLevelArray = ko.observableArray(["Newborn", "Infant", "Toddler", "Homeowner"]);
+	
+	this.nickNames = ko.observableArray([
+			{name: "Steve"}, 
+			{name: "Bert"}
+		]);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
@@ -19,10 +23,10 @@ var ViewModel = function() {
 			
 			return rank = this.catLevelArray()[1];
 		} else {
-			console.log("Happy Star!");
 			return rank = this.catLevelArray()[0];
 		}
 	}, this);
+
 }
 
-ko.applyBindings(new ViewModel);
+ko.applyBindings(new ViewModel());
